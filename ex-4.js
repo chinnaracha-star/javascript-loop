2a46ffe022521950a471552cfc14ab3d1990fac1
Chindurex <chinnaracha@gmail.com>
2026-03-23 16:01:27 +0700

    add answer ex-4

--- ex-4.js
+++ ex-4.js
@@ -3,4 +3,17 @@ let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
 
+for (const x of restaurants){
+    console.log("Restaurant Name:" + x )
+}
 console.log(newRestaurants);
+
+// แบบ ที่ถูก
+// let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
+// let newRestaurants = [];
+
+// for (const x of restaurants){
+//     newRestaurants.push("Restaurant Name: " + x);
+// }
+
+// console.log(newRestaurants);
