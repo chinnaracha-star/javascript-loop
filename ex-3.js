e7dab33946e967afe6509022c50fa6ccf8b52bc5
Chindurex <chinnaracha@gmail.com>
2026-03-23 15:43:58 +0700

    Add answer ex-3

--- ex-3.js
+++ ex-3.js
@@ -12,3 +12,24 @@ for (let i =0 ; i < scores.length; i++){
 
 
 console.log(minScore);
+
+
+
+
+
+// let studentScores = [100, 20, 3, 100];
+
+// // สมมุติให้ตัวแรกเป็นค่าน้อยสุดก่อน
+// let minScore = studentScores[0];
+
+// // เริ่มวนจากตัวที่ 2 (index 1)
+// for (let i = 1; i < studentScores.length; i++) {
+//   // ถ้าค่าปัจจุบันน้อยกว่าค่าน้อยสุดที่เก็บไว้
+//   if (studentScores[i] < minScore) {
+//     // อัปเดตค่าน้อยสุด
+//     minScore = studentScores[i];
+//   }
+// }
+
+// // แสดงผลลัพธ์
+// console.log(minScore);
