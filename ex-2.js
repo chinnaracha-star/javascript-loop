ffec21e6df0257c98bfda45d106eba8a2b6d6cd7
Chindurex <chinnaracha@gmail.com>
2026-03-23 15:19:18 +0700

    add Answer ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,6 +1,19 @@
 // Exercise #2: Sum the Numbers
+// let numbers = [10, 20, 30, 40, 50];
+// let total = 0;
+
+// // Start coding here
+// for (let i = 0; i < numbers.length; i+=1) {
+//   total += numbers[i];
+// }
+
+// console.log(total); 
+
+// Exercise #2: Sum the Numbers
 let numbers = [10, 20, 30, 40, 50];
 let total = 0;
-// Start coding here
 
-console.log(total);
+for (let x = 0 ; x < numbers.length; x+=1){
+    total += numbers[x];
+}
+ console.log(total); 
